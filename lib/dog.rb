0903a70283e4d6eacbb16f256fa8228b6ee07219
Kevin <kevin.kipkoech@student.moringaschool.com>
2022-10-21 15:41:06 +0300

    Completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
+    #setters
+    def name=(name)
+        @name = name
+    end
 
+    def breed=(breed)
+        @breed = breed
+    end
+
+    #getters
+    def name
+        @name
+    end
+
+    def breed
+        @breed
+    end
 end
